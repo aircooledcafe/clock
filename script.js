4a80940cf2d52136aad20f99e51aef65ee1c5598
Ben Lee <ben@HAL9000>
2022-10-28 09:35:29 +0100

    initial comit

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// initiate the variables for the html elements
+const hoursEl = document.querySelector(".hours");
+const minutesEl = document.querySelector(".minutes");
+const secondsEl = document.querySelector(".seconds");
+const dateEl = document.querySelector(".date");
+
+// initiate the variables for the time
+let dateTime, hours, minutes, seconds, date;
+
+const getTime = function () {
+  dateTime = new Date();
+  let timeString = dateTime.toLocaleTimeString();
+  // hours = dateTime.getHours();
+  // minutes = dateTime.getMinutes();
+  // seconds = dateTime.getSeconds();
+  hours = timeString.substring(0, 2);
+  minutes = timeString.substring(3, 5);
+  seconds = timeString.substring(6, 8);
+};
+
+const dateString = function () {
+  dateTime = new Date();
+  let day = dateTime.toLocaleDateString("default", { day: "numeric" });
+  let month = dateTime.toLocaleDateString("default", { month: "long" });
+  let year = dateTime.toLocaleDateString("default", { year: "numeric" });
+  date = `${day} ${month} ${year}`;
+};
+
+// function to get the current tim and set the html elements to the current time.
+const setTime = function () {
+  getTime();
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+  dateString();
+  dateEl.textContent = date;
+};
+
+// update the clock every second
+// setInterval(setTime, 1000);
